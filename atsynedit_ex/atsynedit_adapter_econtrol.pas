eb9b8ba99479b6dad7693d710e9ca38e1a18558e
Alexey-t <support@uvviewsoft.com>
2019-06-30 09:25:37 +0300

    split code to atsynedit_sortedrange.pas

--- atsynedit_ex/atsynedit_adapter_econtrol.pas
+++ atsynedit_ex/atsynedit_adapter_econtrol.pas
@@ -5,7 +5,6 @@ License: MPL 2.0 or LGPL
 unit ATSynEdit_Adapter_EControl;
 
 {$mode objfpc}{$H+}
-{$ModeSwitch advancedrecords}
 
 interface
 
@@ -17,7 +16,7 @@ uses
   ATSynEdit_Adapters,
   ATSynEdit_Carets,
   ATSynEdit_Ranges,
-  ATSynEdit_FGL,
+  ATSynEdit_SortedRange,
   ATStringProc,
   ATStringProc_TextBuffer,
   ATStrings,
@@ -40,35 +39,6 @@ type
     PosBegin: TPoint;
     PosEnd: TPoint;
     procedure Assign(Src: TATRangeInCodeTree);
-  end;
-
-type
-  { TATSortedRange }
-
-  PATSortedRange = ^TATSortedRange;
-  TATSortedRange = record
-    Pos1, Pos2: TPoint;
-    Token1, Token2: integer;
-    Color: TColor;
-    Rule: TecTagBlockCondition;
-    ActiveAlways: boolean;
-    Active: array[0..Pred(cMaxStringsClients)] of boolean;
-    class operator =(const a, b: TATSortedRange): boolean;
-    procedure Init(
-      APos1, APos2: TPoint;
-      AToken1, AToken2: integer;
-      AColor: TColor; ARule: TecTagBlockCondition;
-      AActiveAlways: boolean);
-    function IsPosInside(const APos: TPoint): boolean;
-  end;
-
-  { TATSortedRanges }
-
-  TATSortedRanges = class(specialize TFPGList<TATSortedRange>)
-  public
-    function ItemPtr(AIndex: integer): PATSortedRange; inline;
-    function Find(const APos: TPoint): integer;
-    procedure UpdateOnChange(AChange: TATLineChangeKind; ALine, AItemCount: integer);
   end;
 
   TATRangeCond = (cCondInside, cCondAtBound, cCondOutside);
@@ -205,14 +175,6 @@ const
     cLineStyleDotted
     );
 
-function ComparePoints(const P1, P2: TPoint): integer; inline;
-begin
-  if (P1.X=P2.X) and (P1.Y=P2.Y) then exit(0);
-  if (P1.Y>P2.Y) then exit(1);
-  if (P1.Y<P2.Y) then exit(-1);
-  if (P1.X>P2.X) then exit(1) else exit(-1);
-end;
-
 procedure ClearTreeviewWithData(ATree: TTreeView);
 var
   i: integer;
@@ -255,113 +217,6 @@ begin
   part.BorderRight:= cBorderEc[st.BorderTypeRight];
 end;
 
-{ TATSortedRanges }
-
-function TATSortedRanges.Find(const APos: TPoint): integer;
-
-  function CompProc(ItemIndex: integer): integer; inline;
-  var
-    Item: PATSortedRange;
-  begin
-    Item:= ItemPtr(ItemIndex);
-    if Item^.IsPosInside(APos) then
-      Result:= 0
-    else
-      Result:= ComparePoints(Item^.Pos1, APos);
-  end;
-
-var
-  L, H, I, C, NCount: Integer;
-begin
-  Result := -1;
-  NCount := Count;
-  if NCount = 0 then
-    Exit;
-
-  L := 0;
-  H := NCount - 1;
-  while L <= H do
-  begin
-    I := (L + H) shr 1;
-    C := CompProc(I);
-    if C < 0 then
-      L := I + 1
-    else
-    if C = 0 then
-      Exit(I)
-    else
-      H := I - 1;
-  end;
-
-  Result := L;
-  if Result >= NCount then
-    Result := NCount - 1;
-  if Result >= 0 then
-    if CompProc(Result) > 0 then
-      Dec(Result);
-end;
-
-function TATSortedRanges.ItemPtr(AIndex: integer): PATSortedRange;
-begin
-  Result:= PATSortedRange(InternalGet(AIndex));
-end;
-
-procedure TATSortedRanges.UpdateOnChange(AChange: TATLineChangeKind; ALine, AItemCount: integer);
-var
-  Ptr: PATSortedRange;
-  i: integer;
-begin
-  case AChange of
-    cLineChangeDeletedAll:
-      Clear;
-
-    cLineChangeAdded:
-      begin
-        for i:= Count-1 downto 0 do
-        begin
-          Ptr:= InternalGet(i);
-          if Ptr^.Pos1.Y>=ALine then
-          begin
-            Ptr^.Pos1.Y+= AItemCount;
-            Ptr^.Pos2.Y+= AItemCount;
-          end
-          else
-          if Ptr^.Pos2.Y>=ALine then
-            Ptr^.Pos2.Y+= AItemCount;
-        end;
-      end;
-
-    cLineChangeDeleted:
-      begin
-        for i:= Count-1 downto 0 do
-        begin
-          Ptr:= InternalGet(i);
-          if Ptr^.Pos1.Y>=ALine+AItemCount then
-          begin
-            Ptr^.Pos1.Y-= AItemCount;
-            Ptr^.Pos2.Y-= AItemCount;
-          end
-          else
-          if Ptr^.Pos1.Y>=ALine then
-          begin
-            if Ptr^.Pos2.Y<=ALine+AItemCount then
-              Delete(i)
-            else
-            begin
-              Ptr^.Pos1.Y:= Max(ALine, Ptr^.Pos1.Y-AItemCount);
-              Ptr^.Pos2.Y-= AItemCount;
-            end;
-          end
-          else
-          if Ptr^.Pos2.Y>=ALine then
-          begin
-            Ptr^.Pos2.Y:= Max(ALine, Ptr^.Pos2.Y-AItemCount);
-          end;
-        end;
-      end;
-  end;
-end;
-
 { TATRangeInCodeTree }
 
 procedure TATRangeInCodeTree.Assign(Src: TATRangeInCodeTree);
@@ -370,38 +225,6 @@ begin
   PosEnd:= Src.PosEnd;
 end;
 
-{ TATSortedRange }
-
-class operator TATSortedRange.=(const a, b: TATSortedRange): boolean;
-begin
-  Result:= false;
-end;
-
-procedure TATSortedRange.Init(APos1, APos2: TPoint; AToken1,
-  AToken2: integer; AColor: TColor; ARule: TecTagBlockCondition;
-  AActiveAlways: boolean);
-var
-  i: integer;
-begin
-  Pos1:= APos1;
-  Pos2:= APos2;
-  Token1:= AToken1;
-  Token2:= AToken2;
-  Color:= AColor;
-  Rule:= ARule;
-  ActiveAlways:= AActiveAlways;
-  for i:= Low(Active) to High(Active) do
-    Active[i]:= false;
-end;
-
-function TATSortedRange.IsPosInside(const APos: TPoint): boolean;
-begin
-  Result:= IsPosInRange(
-    APos.X, APos.Y,
-    Pos1.X, Pos1.Y,
-    Pos2.X, Pos2.Y
-    ) = cRelateInside;
-end;
 
 { TATAdapterEControl }
 
---
+++ atsynedit_ex/atsynedit_sortedrange.pas
@@ -0,0 +1,202 @@
+{
+Copyright (C) Alexey Torgashin, uvviewsoft.com
+License: MPL 2.0 or LGPL
+}
+unit ATSynEdit_SortedRange;
+
+{$mode objfpc}{$H+}
+{$ModeSwitch advancedrecords}
+
+interface
+
+uses
+  Classes, SysUtils, Graphics, Math,
+  ATStrings,
+  ATStringProc,
+  ATSynEdit_Carets,
+  ATSynEdit_FGL,
+  ec_SyntAnal;
+
+type
+  { TATSortedRange }
+
+  PATSortedRange = ^TATSortedRange;
+  TATSortedRange = record
+    Pos1, Pos2: TPoint;
+    Token1, Token2: integer;
+    Color: TColor;
+    Rule: TecTagBlockCondition;
+    ActiveAlways: boolean;
+    Active: array[0..Pred(cMaxStringsClients)] of boolean;
+    class operator =(const a, b: TATSortedRange): boolean;
+    procedure Init(
+      APos1, APos2: TPoint;
+      AToken1, AToken2: integer;
+      AColor: TColor; ARule: TecTagBlockCondition;
+      AActiveAlways: boolean);
+    function IsPosInside(const APos: TPoint): boolean;
+  end;
+
+  { TATSortedRanges }
+
+  TATSortedRanges = class(specialize TFPGList<TATSortedRange>)
+  public
+    function ItemPtr(AIndex: integer): PATSortedRange; inline;
+    function Find(const APos: TPoint): integer;
+    procedure UpdateOnChange(AChange: TATLineChangeKind; ALine, AItemCount: integer);
+  end;
+
+function ComparePoints(const P1, P2: TPoint): integer; inline;
+
+implementation
+
+function ComparePoints(const P1, P2: TPoint): integer; inline;
+begin
+  if (P1.X=P2.X) and (P1.Y=P2.Y) then exit(0);
+  if (P1.Y>P2.Y) then exit(1);
+  if (P1.Y<P2.Y) then exit(-1);
+  if (P1.X>P2.X) then exit(1) else exit(-1);
+end;
+
+{ TATSortedRange }
+
+class operator TATSortedRange.=(const a, b: TATSortedRange): boolean;
+begin
+  Result:= false;
+end;
+
+procedure TATSortedRange.Init(APos1, APos2: TPoint; AToken1,
+  AToken2: integer; AColor: TColor; ARule: TecTagBlockCondition;
+  AActiveAlways: boolean);
+var
+  i: integer;
+begin
+  Pos1:= APos1;
+  Pos2:= APos2;
+  Token1:= AToken1;
+  Token2:= AToken2;
+  Color:= AColor;
+  Rule:= ARule;
+  ActiveAlways:= AActiveAlways;
+  for i:= Low(Active) to High(Active) do
+    Active[i]:= false;
+end;
+
+function TATSortedRange.IsPosInside(const APos: TPoint): boolean;
+begin
+  Result:= IsPosInRange(
+    APos.X, APos.Y,
+    Pos1.X, Pos1.Y,
+    Pos2.X, Pos2.Y
+    ) = cRelateInside;
+end;
+
+{ TATSortedRanges }
+
+function TATSortedRanges.Find(const APos: TPoint): integer;
+
+  function CompProc(ItemIndex: integer): integer; inline;
+  var
+    Item: PATSortedRange;
+  begin
+    Item:= ItemPtr(ItemIndex);
+    if Item^.IsPosInside(APos) then
+      Result:= 0
+    else
+      Result:= ComparePoints(Item^.Pos1, APos);
+  end;
+
+var
+  L, H, I, C, NCount: Integer;
+begin
+  Result := -1;
+  NCount := Count;
+  if NCount = 0 then
+    Exit;
+
+  L := 0;
+  H := NCount - 1;
+  while L <= H do
+  begin
+    I := (L + H) shr 1;
+    C := CompProc(I);
+    if C < 0 then
+      L := I + 1
+    else
+    if C = 0 then
+      Exit(I)
+    else
+      H := I - 1;
+  end;
+
+  Result := L;
+  if Result >= NCount then
+    Result := NCount - 1;
+  if Result >= 0 then
+    if CompProc(Result) > 0 then
+      Dec(Result);
+end;
+
+function TATSortedRanges.ItemPtr(AIndex: integer): PATSortedRange;
+begin
+  Result:= PATSortedRange(InternalGet(AIndex));
+end;
+
+procedure TATSortedRanges.UpdateOnChange(AChange: TATLineChangeKind; ALine, AItemCount: integer);
+var
+  Ptr: PATSortedRange;
+  i: integer;
+begin
+  case AChange of
+    cLineChangeDeletedAll:
+      Clear;
+
+    cLineChangeAdded:
+      begin
+        for i:= Count-1 downto 0 do
+        begin
+          Ptr:= InternalGet(i);
+          if Ptr^.Pos1.Y>=ALine then
+          begin
+            Ptr^.Pos1.Y+= AItemCount;
+            Ptr^.Pos2.Y+= AItemCount;
+          end
+          else
+          if Ptr^.Pos2.Y>=ALine then
+            Ptr^.Pos2.Y+= AItemCount;
+        end;
+      end;
+
+    cLineChangeDeleted:
+      begin
+        for i:= Count-1 downto 0 do
+        begin
+          Ptr:= InternalGet(i);
+          if Ptr^.Pos1.Y>=ALine+AItemCount then
+          begin
+            Ptr^.Pos1.Y-= AItemCount;
+            Ptr^.Pos2.Y-= AItemCount;
+          end
+          else
+          if Ptr^.Pos1.Y>=ALine then
+          begin
+            if Ptr^.Pos2.Y<=ALine+AItemCount then
+              Delete(i)
+            else
+            begin
+              Ptr^.Pos1.Y:= Max(ALine, Ptr^.Pos1.Y-AItemCount);
+              Ptr^.Pos2.Y-= AItemCount;
+            end;
+          end
+          else
+          if Ptr^.Pos2.Y>=ALine then
+          begin
+            Ptr^.Pos2.Y:= Max(ALine, Ptr^.Pos2.Y-AItemCount);
+          end;
+        end;
+      end;
+  end;
+end;
+
+end.
+
