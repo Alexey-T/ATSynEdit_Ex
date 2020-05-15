02a5862184affc6041277c1a123e208305bc0e60
Alexey <support@uvviewsoft.com>
2020-05-15 14:50:45 +0300

    for last econtrol

--- atsynedit_ex/atsynedit_adapter_econtrol.pas
+++ atsynedit_ex/atsynedit_adapter_econtrol.pas
@@ -377,8 +377,8 @@ begin
   partindex:= 0;
 
   //startindex:= DoFindToken(Point(0, ALine));
-  if ALine<=High(AnClient.TokenFinder) then
-    startindex:= AnClient.TokenFinder[ALine]
+  if ALine<=High(AnClient.LineIndexer) then
+    startindex:= AnClient.LineIndexer[ALine]
   else
     exit;
 
@@ -1273,8 +1273,8 @@ function TATAdapterEControl.DoFindToken(APos: TPoint): integer; inline;
 begin
   if APos.X=0 then
   begin
-    if APos.Y<=High(AnClient.TokenFinder) then
-      Result:= AnClient.TokenFinder[APos.Y]
+    if APos.Y<=High(AnClient.LineIndexer) then
+      Result:= AnClient.LineIndexer[APos.Y]
     else
       Result:= -1;
   end
